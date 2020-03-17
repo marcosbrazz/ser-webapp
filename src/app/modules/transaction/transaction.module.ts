247ebe6eb7b3c57e4271a006500b40e9bcbcd5c9
Marcos Henrique Braz <mhbraz@br.ibm.com>
2020-03-17 20:17:59 -0300

    Implemented transaction view
    Signed-off-by: Marcos Henrique Braz <mhbraz@br.ibm.com>

--- src/app/modules/transaction/transaction.module.ts
+++ src/app/modules/transaction/transaction.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { TransactionRoutingModule } from './transaction-routing.module';
 import { TransactionComponent } from './transaction/transaction.component';
+import { GridModule, ButtonModule } from 'carbon-components-angular';
 
 
 @NgModule({
   declarations: [TransactionComponent],
   imports: [
     CommonModule,
+    GridModule,
+    ButtonModule,
     TransactionRoutingModule
   ]
 })
