dcafb505bd579a2a621e93a7b1cbce99847a451e
Marcos Henrique Braz <mhbraz@br.ibm.com>
2020-03-29 18:25:19 -0300

    Mocked angular service
    Signed-off-by: Marcos Henrique Braz <mhbraz@br.ibm.com>

--- src/app/shared/services/expense.service.ts
+++ src/app/shared/services/expense.service.ts
@@ -11,6 +11,9 @@ export class ExpenseService {
   constructor(private httpClient: HttpClient) { }
 
   get(): Observable<any> {
-    return this.httpClient.get<any>(`${environment.apiUrl}/api/expense`);
+    const json = '[{"email":"cio_3@ibm.com","vendor":"vale night","vendor_type":"casa rosa","date":"2020-03-16 20:37:11.846307","price":0.01,"user_outlier":false,"usergroup_outlier":false}, \
+    {"email":"cio_3@ibm.com","vendor":"Uber","vendor_type":"Transport","date":"2020-03-16 20:37:11.846307","price":1.23,"user_outlier":false,"usergroup_outlier":false}]';
+    return JSON.parse(json);
+    // return this.httpClient.get<any>(`${environment.apiUrl}/api/expense`);
   }
 }
